bccc354999e58e245cc5a6b4e6f6a5d5f263346a
Nishiy007 <146531476+Nishiy007@users.noreply.github.com>
2024-01-16 16:16:58 +0530

    Dynamic Data

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,14 +1,15 @@
 import './ExpenseItem.css';
-function ExpenseItem() {
-    const expenseDate=new Date(2021, 2, 28);
-    const ExpenseTitle='Car Insurance';
-    const expenseAmount=294.67;
+function ExpenseItem(props) {
+    console.log(props);
+    
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
-            <div classNme='expense-item__description'>
-                <h2>{ExpenseTitle}</h2>
-                <div className='expense-item-price'>${expenseAmount}</div>
+            <div>{props.date.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item-price'>${props.amount}</div>
+                <div className='expense-item-Location'>{props.LocationOfExpenditure}</div>
+                
             </div>
         </div>
     );
